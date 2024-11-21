08aa88726e187b7ccf7351259904d769db2f3626
Dusan Figala <dusanfigala@protonmail.com>
2024-11-21 15:51:49 +0100

    Mosaic all images relevant to given month

--- test-3/cli/mosaic/mosaic.py
+++ test-3/cli/mosaic/mosaic.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import subprocess
 
 import click
@@ -6,11 +7,22 @@ import click
 
 @click.command()
 @click.argument("--intersects")
-@click.argument("--timestamp")
-@click.argument("images", nargs=-1)
-def main(intersects, timestamp, images):
+@click.argument("--month-json")
+@click.argument("all_images", nargs=-1)
+def main(intersects, month_json, all_images):
+    with open(month_json, "r") as f:
+        month_urls = json.load(f)
+
+    # Get image ids relevant to the month
+    month_ids = [item["id"] for item in month_urls]
+
+    timestamp = Path(month_json).stem.split("_")[1]
+
     output_file = f"mosaic_{timestamp}.tif"
-    input_args = [f"-i {img}" for img in images]
+
+    # Get paths to all images relevant to the month
+    month_images = [img for img in all_images if Path(img).stem in month_ids]
+    input_args = [f"-i {img}" for img in month_images]
     intersects = json.loads(intersects)
 
     with open("aoi.geojson", "w") as f:
